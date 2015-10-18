d5c3c012699226bcb5ce3c5089a9ce53f7b60f17
Vladimir Blinov <vovka_squid@mail.ru>
2015-10-18 22:47:21 +0300

    GSM and Wi-Fi network status
    Signed-off-by: Vladimir Blinov <vovka_squid@mail.ru>

--- Course_Project/app/src/main/java/com/vovansoft/course_project/First_Activity.java
+++ Course_Project/app/src/main/java/com/vovansoft/course_project/First_Activity.java
@@ -1,33 +1,155 @@
 package com.vovansoft.course_project;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.TelecomManager;
+import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.telephony.SignalStrength;
+import android.telephony.PhoneStateListener;
 
-public class First_Activity extends AppCompatActivity {
+
+public class First_Activity extends AppCompatActivity
+        implements CompoundButton.OnCheckedChangeListener {
+
+
+
+    private TextView gsmLevel;
+    private TextView wifiLevel;
+    private TextView wifiSpeed;
+    private TextView wifiStatus;
+    private TextView wifiSSID;
+    private CheckBox cbEnable;
+    private WifiManager managerWiFi;
+    private TextView text;
+
+    //Вешаем слушалку
+    private PhoneStateListener listener = new PhoneStateListener() {
+
+        @Override
+        public void onSignalStrengthsChanged(SignalStrength signalStrength) {
+            super.onSignalStrengthsChanged(signalStrength);
+            Integer strength = signalStrength.getGsmSignalStrength();
+            //Если произошло событие, то закидываем
+            gsmLevel.setText(strength.toString());
+        }
+    };
+
+    private BroadcastReceiver receiver = new BroadcastReceiver(){
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int wifiState = intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE,
+                    WifiManager.WIFI_STATE_UNKNOWN);
+
+            switch(wifiState){
+                case WifiManager.WIFI_STATE_ENABLING:
+                    wifiStatus.setText("Wi-Fi state enabling");
+                    break;
+                case WifiManager.WIFI_STATE_ENABLED:
+                    wifiStatus.setText("Wi-Fi state enabled");
+                    startMonitoringRssi();
+                    break;
+                case WifiManager.WIFI_STATE_DISABLING:
+                    wifiStatus.setText("Wi-Fi state disabling");
+                    wifiSpeed.setText("");
+                    wifiSSID.setText("");
+                    wifiLevel.setText("");
+                    break;
+                case WifiManager.WIFI_STATE_DISABLED:
+                    wifiStatus.setText("Wi-Fi state disabled");
+                    stopMonitoringRssi();
+                    wifiSpeed.setText("");
+                    wifiSSID.setText("");;
+                    wifiLevel.setText("");
+
+                    break;
+                case WifiManager.WIFI_STATE_UNKNOWN:
+                    wifiStatus.setText("Wi-Fi state unknown");
+                    wifiSpeed.setText("");
+                    wifiSSID.setText("");
+                    wifiLevel.setText("");
+                    break;
+            }
+        }
+    };
+
+    private BroadcastReceiver rssiReceiver = new BroadcastReceiver(){
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            WifiInfo info = managerWiFi.getConnectionInfo();
+
+           /* wifiSSID.append("\nChange signal in " + info.getSSID());
+            wifiSSID.append("\n\tSignal level:\t" +
+                    WifiManager.calculateSignalLevel(info.getRssi(), 5));
+            wifiSSID.append("\n\tLink speed:\t" + info.getLinkSpeed() +
+                    " " + WifiInfo.LINK_SPEED_UNITS);*/
+            Integer wifiIntSignal = WifiManager.calculateSignalLevel(info.getRssi(), 100);
+            Integer wifiIntSpeed = info.getLinkSpeed();
+            wifiSSID.setText(info.getSSID());
+            wifiSpeed.setText(wifiIntSpeed.toString() +  WifiInfo.LINK_SPEED_UNITS);
+            wifiLevel.setText(wifiIntSignal.toString());
+        }
+    };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_);
-        textView = (TextView) findViewById(R.id.textView);
-        button = (Button) findViewById(R.id.button2);
+        //GSM network
+        TelephonyManager manager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        gsmLevel = (TextView) findViewById(R.id.gsmLevel);
+        wifiLevel = (TextView) findViewById(R.id.wifiLevel);
+        wifiSpeed = (TextView)findViewById(R.id.wifiSpeed);
+        wifiSSID =(TextView) findViewById(R.id.wifiSSID);
+        wifiStatus = (TextView) findViewById(R.id.wifiStatus);
+        manager.listen(listener,PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText("Ты кто?");
-            }
-        });
+        //Wi-Fi network
+        text = (TextView)findViewById(R.id.text);
+        cbEnable = (CheckBox)findViewById(R.id.cbEnable);
+
+        managerWiFi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+
+
+        this.registerReceiver(this.receiver,
+                new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION));
+
+        cbEnable.setChecked(managerWiFi.isWifiEnabled());
+        cbEnable.setOnCheckedChangeListener(this);
+
+
+
     }
 
-    public void Say_Hello(View view) {
-        textView.setText("Привет!");
+    @Override
+    public void onCheckedChanged(CompoundButton view, boolean isChecked) {
+        managerWiFi.setWifiEnabled(isChecked);
     }
+
+    private void startMonitoringRssi() {
+        this.registerReceiver(rssiReceiver,
+                new IntentFilter(WifiManager.RSSI_CHANGED_ACTION));
+    }
+
+    private void stopMonitoringRssi() {
+        if (this.rssiReceiver.isInitialStickyBroadcast())
+            this.unregisterReceiver(rssiReceiver);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,8 +158,6 @@ public class First_Activity extends AppCompatActivity {
         return true;
     }
 
-    private TextView textView;
-    private Button button;
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
